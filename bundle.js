f7336a62820548f06eb1ec82994c604bbf530036
IMLaing <ianmlaing@gmail.com>
2016-10-20 22:42:44 -0400

    added a method to the url

--- bundle.js
+++ bundle.js
@@ -415,7 +415,7 @@
 	}
 
 	exports.searchFlickr = function(){
-	    var url = "https://api.flickr.com/services/flickr.photos.search/?";
+	    var url = "https://api.flickr.com/services/?method=flickr.photos.search";
 	    var parameters = {
 	        api_key: "92fc59789ddf2029562e91f4719d5ee4",
 	        text: "cats",
--- content.js
+++ content.js
@@ -6,7 +6,7 @@ exports.sayHello = function() {
 }
 
 exports.searchFlickr = function(){
-    var url = "https://api.flickr.com/services/flickr.photos.search/?";
+    var url = "https://api.flickr.com/services/?method=flickr.photos.search";
     var parameters = {
         api_key: "92fc59789ddf2029562e91f4719d5ee4",
         text: "cats",
