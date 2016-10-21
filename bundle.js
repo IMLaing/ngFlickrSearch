9657686330e1e43d117482f7d0ff98bb3c5effb8
IMLaing <ianmlaing@gmail.com>
2016-10-20 22:53:58 -0400

    reverted

--- bundle.js
+++ bundle.js
@@ -407,24 +407,26 @@
 /* 5 */
 /***/ function(module, exports, __webpack_require__) {
 
+	console.log('content.js loaded')
+
 	var $ = __webpack_require__(6);
 	var exports = module.exports = {};
-	console.log('does this load?');
+
 	exports.sayHello = function() {
 	    return "hello";
 	}
 
 	exports.searchFlickr = function(){
-	    console.log('we have searchFlickr');
-	    var url = "https://api.flickr.com/services/?method=flickr.photos.search";
+	    console.log('searchF ran');
+	    var url = "https://api.flickr.com/services/rest/?method=flickr.photos.search";
 	    var parameters = {
 	        api_key: "92fc59789ddf2029562e91f4719d5ee4",
-	        tags: "cats",
+	        text: "cats",
 	        format: 'json',
 	        per_page: 20
 	    }
-	    
-	    $.getJSON(url+'&jsoncallback=?', parameters, function(result){
+	    console.log('$.getJson next');
+	    $.getJSON(url, parameters, function(result){
 	        console.log(result);
 	        console.log('you did it!');
 	    })
