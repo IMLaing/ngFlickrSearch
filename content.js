779ffc1e910a55d85af25a0c08fba6019073564e
IMLaing <ianmlaing@gmail.com>
2016-10-20 23:19:11 -0400

    using server

--- content.js
+++ content.js
@@ -1,31 +1,30 @@
-console.log('content.js loaded')
+console.log('content.js loaded');
 
 var $ = require("jquery");
 var exports = module.exports = {};
 
-exports.sayHello = function() {
+exports.sayHello = function () {
     return "hello";
-}
+};
 
-exports.searchFlickr = function(){
+exports.searchFlickr = function () {
     console.log('searchF ran');
-    var url = "https://api.flickr.com/services/rest/";
     var parameters = {
+        url:"https://api.flickr.com/services/rest/"
         api_key: "92fc59789ddf2029562e91f4719d5ee4",
         text: "cats",
         method: 'flickr.photos.search',
         format: 'json',
         per_page: 20,
         jsoncallback: '?',
-        jsonp: 'jsoncallback'
-    }
-    var fullUrl = url + '?jsoncallback=?';
-    var jsonFlickrApi = function(){};
+        jsonp: 'jsoncallback',
+        nojsoncallback: 1 
+    };
     console.log('$.getJson next');
-    $.getJSON(fullUrl, parameters, function(result)){
+    $.getJSON(url, parameters, function (result){ 
         console.log(result);
         console.log('you did it!');
-    })
+    });
 
 }
 
