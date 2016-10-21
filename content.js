2b66c98ddc04f16f2bc72ee047abed6a9b93cad4
IMLaing <ianmlaing@gmail.com>
2016-10-20 21:54:15 -0400

    updated js

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+var $ = require("jquery");
 var exports = module.exports = {};
 
 exports.sayHello = function() {
@@ -7,8 +8,18 @@ exports.sayFu = function() {
     return "Get the frack out of here!";
 }
 
+exports.searchFlickr = function(){
+    var url = "https://api.flickr.com/services";
+    var parameters = {
+        api_key: "92fc59789ddf2029562e91f4719d5ee4",
+        text: 'cats',
+        per_page: 20
+    }
+    
+    $.getJSON(url, parameters, function(result){
+        console.log(result);
+    })
 
-
-
+}
 
 
--- entry.js
+++ entry.js
@@ -2,3 +2,4 @@ require("./style.css");
 var contentjs = require("./content.js");
 
 console.log(contentjs.sayHello()); // content.js has is referenced by contentjs.
+contentjs.searchFlickr();
