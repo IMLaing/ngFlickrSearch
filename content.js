a604ebfed9bed88529a2769744503550050c90b1
IMLaing <ianmlaing@gmail.com>
2016-10-20 21:30:10 -0400

    webpack ready first commit

---
+++ content.js
@@ -0,0 +1,14 @@
+var exports = module.exports = {};
+
+exports.sayHello = function() {
+    return "hello";
+}
+exports.sayFu = function() {
+    return "Get the frack out of here!";
+}
+
+
+
+
+
+
---
+++ entry.js
@@ -0,0 +1,4 @@
+require("./style.css");
+var contentjs = require("./content.js");
+
+console.log(contentjs.sayHello()); // content.js has is referenced by contentjs.
